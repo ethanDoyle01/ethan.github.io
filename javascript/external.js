ee4b519eee94f3bce8f615c1683ea509573f7bfe
ethanDoyle01 <ethancdoyle0620@gmail.com>
2025-09-22 15:21:34 -0500

    Completed Task 6 Array

--- javascript/external.js
+++ javascript/external.js
@@ -1,10 +1,5 @@
-document.writeln("<button onclick='condition()'>Conditional Test</button>");
-function condition() {
-    x = confirm("Are you sure you want to proceed?");
-    if (x) {
-        document.writeln("You chose Okay!");
-    }
-    else {
-        document.writeln("You chose Cancel!");
-    }
+arr = ["Mael","Eric","Randall","James"];
+for (var i=0; i < arr.length; i++)
+{
+document.writeln(arr[i],"<br>");
 }
